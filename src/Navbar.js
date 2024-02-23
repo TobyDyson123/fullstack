0aece381762e2c8d3c4e6e972935d0e77f582f71
TobyDyson123 <p110138434@student.peterborough.ac.uk>
2024-02-23 14:06:50 +0000

    added navbar

---
+++ src/Navbar.js
@@ -0,0 +1,27 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <nav class="navbar">
+            <div class="brand-title">Brand</div>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+            <div class="navbar-links">
+                <ul>
+                <li><a href="#">Announcements</a></li>
+                <li><a href="#">Instructors</a></li>
+                <li><a href="#">Classes</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign Up</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
