4645269792d97369160ba9b2477150ea1854ae8d
TobyDyson123 <p110138434@student.peterborough.ac.uk>
2024-02-25 12:44:00 +0000

    added images

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import Navbar from './navbar';
 import Hero from './hero';
+import Mission from './mission';
 
 function Home() {
   return (
     <div className="Home">
         <Navbar />
         <Hero />
+        <Mission />
     </div>
   );
 }
---
+++ src/mission.js
@@ -0,0 +1,14 @@
+import './mission.css';
+
+function Mission() {
+    return (
+      <div className="Mission">
+          <div className="Content">
+            <h1>Our Mission</h1>
+            
+          </div>
+      </div>
+    );
+  }
+  
+  export default Mission;
