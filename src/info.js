d2fe9650cf0bdf7ec95139b0e7962e040226770d
TobyDyson123 <p110138434@student.peterborough.ac.uk>
2024-02-25 16:10:20 +0000

    made info responsive

--- src/info.js
+++ src/info.js
@@ -1,4 +1,5 @@
 import './info.css';
+import instructor from './Images/instructor.webp';
 
 function Info() {
     return (
@@ -14,13 +15,35 @@ function Info() {
                     Our instructors are not only certified through rigorous training programs, 
                     but they also embody the principles of yoga in their daily lives.
                 </p>
+                <a href='#'>Meet the team</a>
             </div>
             <div className='top-container-image'>
-                <img src='' alt='instructors' />
+                <img src={instructor} alt='instructors' />
             </div>
           </div>
           <div className='bottom-container'>
             <h1>What We Offer</h1>
+            <div className='bottom-container-cards'>
+                <div className='card'>
+                    <img src={instructor} alt='instructors' />
+                    <p>
+                        Fun, interactive classes led by trained professionals
+                    </p>
+                </div>
+                <div className='card'>
+                    <img src={instructor} alt='instructors' />
+                    <p>
+                        Client accounts to manage bookings and workshops
+                    </p>
+                </div>
+                <div className='card'>
+                    <img src={instructor} alt='instructors' />
+                    <p>
+                        Email newsletter keeping you up to date with the latest yoga news
+                    </p>
+                </div>
+            </div>
+            <a href='#'>View classes</a>
           </div>
       </div>
     );
