ef189d8e41952ba6e7897e863ff5dd2a8daede45
TobyDyson123 <95701915+TobyDyson123@users.noreply.github.com>
2024-03-10 18:09:44 +0000

    made booked page responsive

--- client/src/Booked.js
+++ client/src/Booked.js
@@ -4,13 +4,20 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './booked.css';
 
-const Bookings = () => {
+const Booked = () => {
 
     return (
         <div className="Booked">
           <Navbar />
-          <div className="bookings-container">
+          <div className="booked-container">
             <h1>You're All Set!</h1>
+            {/* <h2>Booking Confirmation:</h2> */}
+            <p>Thank you for booking with us, we look forward to welcoming you!</p>
+            {/* Class details go here */}
+            <div className="links-container">
+                <Link className="major" to="/classes">Book Another Class</Link>
+                <Link className="minor" to="/bookings">View Bookings</Link>
+            </div>
           </div>
           <Footer />
         </div>
